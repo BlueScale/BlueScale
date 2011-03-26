b8b238f4f2a5fdcb2fe45783aa0e65736a2888a9
Vincent Marquez <vincent.marquez@gmail.com>
2011-03-25 18:01:32 -0700

    added Python example

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 #BlueScale
+
 BlueScale is an Open Source platform allowing web developers to create telcom and VoIP related products or services.
 It uses a simple REST front end to control the scalabe SIP server at it's core.  
 
 
 
 ## Launching
+
 After unzipping the distribution, you need to create a BlueScaleConfig.xml file by copying the example BlueScaleConfig.Sample.xml. 
-ListeningAddress and ListeningPort tag values must be a local IP and Port the server can bind to,
+ListeningAddress and ListeningPort tag values must be a local IP and Port the server can bind to.
 
-ContactAddress must be the external IP your SIP trunking Provider will besending SIP packets to, and needs to be somehow forwarding to your machine.
-If your machine only has an external IP (which would be weird...) this can be the same as the ListeningAddress.
+ContactAddress must be the external IP your SIP trunking Provider will be sending SIP to, and needs to be forwarding to your machine.
 
-DestAddress and DsestPort must be the gateway of your SIP trunking provider.
+DestAddress and DestPort must be the gateway of your SIP trunking provider.
 
 CallbackUrl should point to the address of your web application that BlueScale will post to when an incoming call occurs. 
 
---
+++ resources/examples/IncomingForward.py
@@ -0,0 +1,60 @@
+import string, cgi, time
+import sys
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+number =""
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        println("not supported") 
+        return
+
+    def do_POST(self):
+        if self.path == "/Status":
+            self.handleStatus()
+        else: 
+            self.handleIncomingCall()   
+
+    def handleStatus(self):
+        params = self.parseParams()
+        for field in params.keys():
+            print( field + "=" + "".join(params[field]))
+    
+    def handleIncomingCall(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        str = """
+                <Response>
+                    <Dial>
+                        <Number>
+                        """ + number + """
+                        </Number>
+                        <Action>http://localhost:8100/Status</Action>
+                    </Dial>
+                </Response>
+            """
+        self.wfile.write(str)
+        return
+    
+    def parseParams(self):
+        length = int(self.headers.getheader('Content-Length'))
+        params = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        return params
+    
+
+
+def main():
+    try:
+        server = HTTPServer( ('', 8080), MyHandler)
+        print("started, forwarding to " + number)
+        server.serve_forever()
+        print("serving...")
+        #time.sleep(5000)
+    except Exception, err:
+        print("damn error = " + str(err))
+
+if __name__ == '__main__':
+    number = sys.argv[1]
+    main()
